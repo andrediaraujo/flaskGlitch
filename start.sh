198f0fc5f35d288c8d86095f7bf990baf69454e2
inatel <inatel@gmail.com>
2025-01-13 18:18:38 -0300

    first Version

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-gunicorn hello:app -w 1 --log-file -
+#!/bin/bash
+# Activate virtual environment
+source venv/bin/activate
+
+# Run the Flask app with a production-ready WSGI server like Gunicorn
+exec gunicorn -w 4 -b 0.0.0.0:8000 hello:app
